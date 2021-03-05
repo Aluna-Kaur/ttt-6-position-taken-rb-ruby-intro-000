abe1fd1bf96cb67558329580149352fac148c4c9
Aluna Kaur <aluna.singh89@gmail.com>
2021-03-05 04:03:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,10 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == "" || " "
-    false
-  elsif board[index] == "X" || "O"
+
+  if board[index] == "X" || "O"
     true
-  else
-    true
+  elsif board[index] == "" || " "
+      false
   end
 end
